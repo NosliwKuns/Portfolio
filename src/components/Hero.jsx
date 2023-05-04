3ee4b35aa06f7d749018d38501538b90ba1c0cac
sky <quispealaya73@gmail.com>
2023-05-04 17:35:42 -0500

    add central island

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto'>
+    <section className='relative w-full h-screen mx-auto border'>
       <div 
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
@@ -17,7 +17,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText}`}>Hi, I'm 
-            <span className='text-[#915eff]'>Antoni</span>
+            <span className='text-[#915eff]'> Antoni</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className='sm:block hidden'/>
